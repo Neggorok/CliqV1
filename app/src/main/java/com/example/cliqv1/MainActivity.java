21ea75f71e63b755dd18014d0a4e1fba0b799b08
Neggorok <alexhermes87@gmx.de>
2019-04-30 15:42:41 +0200

    Klassen :  - Profile  - Groupchat

---
+++ app/src/main/java/com/example/cliqv1/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.cliqv1;
+
+import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText userName;
+    EditText userPassword;
+
+    RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Lädt die TextViews der Eingabezeilen, um sie in der Methode nutzen zu können
+        // Eine Id wird auf dem Server automatisch per Script angelegt
+        userName = findViewById(R.id.edittext_name);
+        userPassword = (EditText) findViewById(R.id.edittext_password);
+
+        queue = Volley.newRequestQueue(this);
+    }
+
+    // Das View view sorgt dafür, das Die Methode erkennt das es sich um einen Button handelt
+    public void signUp(View view) {
+
+        // sorgt dafür, das ein StringRequest, also eine Anfrage an den Server gestellt wird
+        String create_user_url = getString(R.string.cliq) + "/registrierung_cliq.php";
+
+        // der Request prüft ob die Daten des Scripts "/registrierung_cliq.php" correct sind
+        StringRequest postRequest = new StringRequest(Request.Method.POST, create_user_url,
+
+                // stellt die Antwort des Serves dar
+                response -> {
+
+                    // gibt die Antwort des Serves in der AS Console aus
+                    // dient nur zur Kontrolle
+                    Log.i("response", response);
+
+                    // der Try - catch bereich funktioniert ähnlich wie eine If Abfrage
+                    // es wird im Try Bereich versucht auf die Antwort zu reagieren
+                    // und sollte die Antwort des Servers die Erwartete sein, reagiert der Catch Bereich dementsprechend
+                    try {
+
+                        // wandelt die Antwort des Servers in JSON um
+                        JSONObject jsonResponse = new JSONObject(response);
+
+                        // der Toast nimmt die Antwort des Servers und gibt diese für den Nutzer in der App sichtbar aus
+                        Toast.makeText(MainActivity.this, jsonResponse.get("message").toString(), Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }, error -> {
+
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                // Hier werden die Eingaben aus den EditTexten userName und userPassword ausgelesen
+                // und in die entsprechenden Variablen geladen
+                params.put("username", userName.getText().toString());
+                params.put("password", userPassword.getText().toString());
+
+                return params;
+            }
+        };
+
+// Add the request to the RequestQueue.
+        queue.add(postRequest);
+    }
+
+    // Das View view sorgt dafür, das Die Methode erkennt das es sich um einen Button handelt
+    public void logIn(View view) {
+        // sorgt dafür, das ein StringRequest, also eine Anfrage an den Server gestellt wird
+        String create_user_url = getString(R.string.cliq) + "/login_cliq.php";
+
+        // der Request prüft ob die Daten des Scripts "/registrierung_cliq.php" correct sind
+        StringRequest postRequest = new StringRequest(Request.Method.POST, create_user_url,
+
+                // stellt die Antwort des Serves dar
+                response -> {
+
+                    // gibt die Antwort des Serves in der AS Console aus
+                    // dient nur zur Kontrolle
+                    Log.i("response", response);
+
+                    // der Try - catch bereich funktioniert ähnlich wie eine If Abfrage
+                    // es wird im Try Bereich versucht auf die Antwort zu reagieren
+                    // und sollte die Antwort des Servers die Erwartete sein, reagiert der Catch Bereich dementsprechend
+                    try {
+
+                        // wandelt die Antwort des Servers in JSON um
+                        JSONObject jsonResponse = new JSONObject(response);
+
+                        // der Toast nimmt die Antwort des Servers und gibt diese für den Nutzer in der App sichtbar aus
+                        Toast.makeText(MainActivity.this, jsonResponse.get("message").toString(), Toast.LENGTH_SHORT).show();
+
+                        int success = Integer.parseInt(jsonResponse.get("success").toString());
+
+                        if(success == 1){
+
+                            PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit().putInt("id", jsonResponse.getInt("user_id")).apply();
+
+                            PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit().putString("username", userName.getText().toString()).apply();
+
+                            PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit().putString("image", jsonResponse.getString("user_image")).apply();
+
+
+                            Intent intent = new Intent(getApplicationContext(), UserListActivity.class);
+                            startActivity(intent);
+
+
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }, error -> {
+
+
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                // Hier werden die Eingaben aus den EditTexten userName und userPassword ausgelesen
+                // und in die entsprechenden Variablen geladen
+                params.put("username", userName.getText().toString());
+                params.put("password", userPassword.getText().toString());
+
+                return params;
+            }
+        };
+
+// Add the request to the RequestQueue.
+        queue.add(postRequest);
+    }
+}
---
+++ app/src/main/java/com/example/cliqv1/UserListActivity.java
@@ -0,0 +1,153 @@
+package com.example.cliqv1;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserListActivity extends AppCompatActivity {
+
+    private UserListAdapter adapter;
+    private List<User> userList;
+
+    RecyclerView userRecyclerView;
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        userList = new ArrayList<>();
+        adapter = new UserListAdapter(this, userList);
+        userRecyclerView = (RecyclerView) findViewById(R.id.userList_recycler_view);
+        userRecyclerView.setHasFixedSize(true);
+        userRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        userRecyclerView.setAdapter(adapter);
+
+        queue = Volley.newRequestQueue(this);
+
+        loadUserList();
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadUserList();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+    }
+
+    public void loadUserList() {
+
+        userList.clear();
+
+        String create_user_url = getString(R.string.cliq) + "/getAllUsers_cliq.php";
+
+        StringRequest postRequest = new StringRequest(Request.Method.GET, create_user_url,
+                response -> {
+
+
+                    Log.i("response", response);
+
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        JSONArray userArray = (JSONArray) jsonResponse.get("user");
+
+                        for (int i = 0; i < userArray.length(); i++) {
+
+                            JSONObject userJson = userArray.getJSONObject(i);
+
+                            if (userJson.getInt("id") != PreferenceManager.getDefaultSharedPreferences(UserListActivity.this).getInt("id", -1)) {
+
+                                if (!userJson.getString("image").equals("null") && userJson.getString("image").length() > 0) {
+
+                                    String bitmapString = userJson.getString("image");
+                                    Bitmap imageBitmap = Util.getBitmapFromBase64String(bitmapString);
+
+                                    userList.add(new User(userJson.getString("username"), imageBitmap));
+                                } else {
+                                    userList.add(new User(userJson.getString("username"), null));
+                                }
+
+                            }
+
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    adapter.notifyDataSetChanged();
+
+                }, error -> {
+
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(postRequest);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_user_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.logout) {
+
+            PreferenceManager.getDefaultSharedPreferences(this).edit().putInt("id", 0).apply();
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+            Toast.makeText(this, "Logout successful", Toast.LENGTH_SHORT).show();
+        }
+
+        if (id == R.id.profile) {
+
+//            Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
+//            startActivity(i);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+
+
+
