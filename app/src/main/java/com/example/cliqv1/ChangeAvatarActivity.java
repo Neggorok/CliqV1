dac32df04cfa423c6791b7a7fcb04800222c4adb
PrincessVika <viktoria.gaulik@stud.hshl.de>
2019-05-22 09:57:14 +0200

    Layout geändert, Buttonfarbe geändert

--- app/src/main/java/com/example/cliqv1/ChangeAvatarActivity.java
+++ app/src/main/java/com/example/cliqv1/ChangeAvatarActivity.java
@@ -27,52 +27,52 @@ public class ChangeAvatarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_avatar);
 
-        imageView = findViewById(R.id.profileImageView);
+      /*  imageView = findViewById(R.id.profileImageView);
         imageButton = findViewById(R.id.imageButton1);
         imageButton = findViewById(R.id.imageButton2);
         imageButton = findViewById(R.id.imageButton3);
         chooseImage = findViewById(R.id.choose_image_btn);
 
-        //       chooseImage.setOnClickListener(new View.OnClickListener() {
-        //           public void onClick(View v) {
-        //               if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        //                   if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-        //                           == PackageManager.PERMISSION_DENIED) {
-        //                       String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-        //                      requestPermissions(permissions, PERMISSION_CODE);
-        //                } else {
-        //                    pickFromGallery();
-        //                 }
-        //          } else {
-        //              pickFromGallery();
-        //          }
+        /*       chooseImage.setOnClickListener(new View.OnClickListener() {
+                   public void onClick(View v) {
+                       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                           if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                                   == PackageManager.PERMISSION_DENIED) {
+                               String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                              requestPermissions(permissions, PERMISSION_CODE);
+                        } else {
+                            pickFromGallery();
+                         }
+                  } else {
+                      pickFromGallery();
+                  }
 
-        //       }
-        // }
-        // private void pickFromGallery() {
-        //    Intent intent = new Intent(Intent.ACTION_PICK);
-        //      intent.setType("image/*");
-        //   startActivity(intent, IMAGE_PICK_CODE);
-        //}
+               }
+         }
+         private void pickFromGallery() {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+              intent.setType("image/*");
+           startActivity(intent, IMAGE_PICK_CODE);
+        }
 
-        // public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int grantResults) {
-        //      switch (requestCode) {
-        //         case PERMISSION_CODE: {
-        //             if (grantResults.lenght >0 && grantResults[0] ==
-        //             PackageManager.PERMISSION_GRANTED) {
-        //                pickFromGallery();
-        //            }
-        //            else {
-        //               Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show();
-        //           }
-        //      }
-        //   }
-        // }
+         public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int grantResults) {
+              switch (requestCode) {
+                 case PERMISSION_CODE: {
+                     if (grantResults.lenght >0 && grantResults[0] ==
+                     PackageManager.PERMISSION_GRANTED) {
+                        pickFromGallery();
+                    }
+                    else {
+                       Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show();
+                   }
+              }
+           }
+         }
 
-        // public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-        //     ImageView.setImageURI(data.getData());
-        //  }
-        // }
+         public void onActivityResult(int requestCode, int resultCode, Intent data) {
+         if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+             ImageView.setImageURI(data.getData());
+          }
+         } */
 }
 }
