0187a0d928e90f653362f52911a64cb06b25c725
artens12 <amri.jedir@stud.hshl.de>
2019-05-22 11:07:29 +0200

    Fixing AudioActivity, now it works only standalone

--- app/src/main/java/com/example/cliqv1/AudioActivity.java
+++ app/src/main/java/com/example/cliqv1/AudioActivity.java
@@ -1,20 +1,27 @@
 package com.example.cliqv1;
 
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import java.io.IOException;
 
+import android.support.v4.app.ActivityCompat;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class AudioActivity extends AppCompatActivity {
 
     private Button record, stop, play;
     private MediaRecorder myAudioRecorder;
     private String outputFile;
+    public static final int RequestPermissionCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +33,40 @@ public class AudioActivity extends AppCompatActivity {
         stop.setEnabled(false);
         play.setEnabled(false);
 
-        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
 
-        myAudioRecorder = new MediaRecorder();
-        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        myAudioRecorder.setOutputFile(outputFile);
 
-        record.setOnClickListener(new View.OnClickListener() {
+
+
+
+        record.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
-                try {
-                    myAudioRecorder.prepare();
-                    myAudioRecorder.start();
-                } catch (IllegalStateException ise) {
+        if(checkPermission()) {
+            outputFile = Environment.getExternalStorageDirectory().getAbsolutePath()
+                    + "/recording.3gp";
 
-                } catch (IOException e) {
+            MediaRecorderReady();
 
-                }
+            try {
 
-                record.setEnabled(false);
-                stop.setEnabled(true);
+                myAudioRecorder.prepare();
+                myAudioRecorder.start();
+            } catch (IllegalStateException e)
+            {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+            record.setEnabled(false);
+            stop.setEnabled(true);
+
+            Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+        }
+        else {
+            requestPermission();
+        }
             }
         });
 
@@ -80,12 +97,56 @@ public class AudioActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Playing Audio...", Toast.LENGTH_LONG).show();
 
                 } catch (Exception e) {
-
+                    e.printStackTrace();
                 }
             }
         });
 
     }
+
+    public void MediaRecorderReady(){
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (grantResults.length> 0) {
+                    boolean StoragePermission = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    boolean RecordPermission = grantResults[1] ==
+                            PackageManager.PERMISSION_GRANTED;
+
+                    if (StoragePermission && RecordPermission) {
+                        Toast.makeText(AudioActivity.this, "Permission Granted",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(AudioActivity.this, "Permission Denied",Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(AudioActivity.this, new
+                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
+    }
+
+    public boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
+                WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
+                RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED &&
+                result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
 
 
 }
