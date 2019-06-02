beaa9e7ec9a1e099dcdf1d58d69f7b9458069ba2
PrincessVika <viktoria.gaulik@stud.hshl.de>
2019-06-02 19:16:20 +0200

    beim Profil wird der Name angezeigt, edit-image hinzugefügt um im späteren Verlauf den Avatar zu ändern ( erst über Kamera )

--- app/src/main/java/com/example/cliqv1/ProfileFragment.java
+++ app/src/main/java/com/example/cliqv1/ProfileFragment.java
@@ -1,14 +1,17 @@
 package com.example.cliqv1;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,6 +33,7 @@ public class ProfileFragment extends Fragment {
     private DatabaseReference profileRef;
     private String currentUserName, currentUserID;
     private FirebaseAuth firebaseAuth;
+    private static final int galleryPick = 1;
 
 
     public ProfileFragment() {
@@ -41,6 +45,44 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        userProfile = getView().findViewById(R.id.editPic);
+        userName = getView().findViewById(R.id.name);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUserID = firebaseAuth.getCurrentUser().getUid();
+        profileRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID).child("name");
+
+        profileRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userName.setText(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        userProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iGallery = new Intent();
+                iGallery.setAction(Intent.ACTION_GET_CONTENT);
+                iGallery.setType("image/*");
+                startActivityForResult(iGallery, galleryPick );
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
 
