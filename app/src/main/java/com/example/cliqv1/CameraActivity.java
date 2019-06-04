a16b3a6fe4ba269cb461b7354c59c3c478a307de
vfroehlich <valeria.froehlich@stud.hshl.de>
2019-06-04 20:22:40 +0200

    PopUp ready :)

--- app/src/main/java/com/example/cliqv1/CameraActivity.java
+++ app/src/main/java/com/example/cliqv1/CameraActivity.java
@@ -94,6 +94,7 @@ public class CameraActivity extends AppCompatActivity {
             }
         });
 
+        
      /*   btnCropImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
