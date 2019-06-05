90a0e8197b4909356df8d4056e9b01c4ab808bc2
vfroehlich <valeria.froehlich@stud.hshl.de>
2019-06-05 10:12:25 +0200

    Logout hinzugefügt & ProfileSettings

--- app/src/main/java/com/example/cliqv1/ChangeEmailActivity.java
+++ app/src/main/java/com/example/cliqv1/ChangeEmailActivity.java
@@ -1,7 +1,12 @@
 package com.example.cliqv1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ChangeEmailActivity extends AppCompatActivity {
 
@@ -10,4 +15,32 @@ public class ChangeEmailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_email);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_shortcut_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.back_to_profile) {
+
+            finish();
+            return true;
+
+        }
+
+        if (id == R.id.set_logout) {
+
+            Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(i);
+            Toast.makeText(ChangeEmailActivity.this, "Logout successful",  Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
+
