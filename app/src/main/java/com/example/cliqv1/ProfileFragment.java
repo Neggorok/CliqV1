e5ee70162c3d88631d5d9f5d312368177d839b60
vfroehlich <valeria.froehlich@stud.hshl.de>
2019-06-05 11:55:25 +0200

    Switches einfügen

--- app/src/main/java/com/example/cliqv1/ProfileFragment.java
+++ app/src/main/java/com/example/cliqv1/ProfileFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
