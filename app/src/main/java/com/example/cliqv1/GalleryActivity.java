f1b3a4d7421602d7612cc67e03bb05ff6fa18342
vfroehlich <valeria.froehlich@stud.hshl.de>
2019-06-05 14:35:17 +0200

    V2 auf neusten Stand bringen

---
+++ app/src/main/java/com/example/cliqv1/GalleryActivity.java
@@ -0,0 +1,14 @@
+package com.example.cliqv1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class GalleryActivity extends AppCompatActivity
+
+{
+
+    private static final int galleryPick = 1;
+
+
+}
