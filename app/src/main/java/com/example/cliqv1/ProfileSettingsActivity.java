3a21307abd7045e0f4af202585f896d1236a2c4a
vfroehlich <valeria.froehlich@stud.hshl.de>
2019-06-05 12:24:21 +0200

    Switches einfügen & Profileinstellungen verbessert

--- app/src/main/java/com/example/cliqv1/ProfileSettingsActivity.java
+++ app/src/main/java/com/example/cliqv1/ProfileSettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.cliqv1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +14,7 @@ public class ProfileSettingsActivity extends AppCompatActivity {
     Button seeConsentForm;
     Button changeEmail;
     Button changePassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
