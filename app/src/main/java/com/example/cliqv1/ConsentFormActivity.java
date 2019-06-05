1585fce16989c42090effba83ff1e1504a7c1f19
vfroehlich <valeria.froehlich@stud.hshl.de>
2019-06-05 10:26:41 +0200

    ConsentForm hinzugefügt hinzugefügt

--- app/src/main/java/com/example/cliqv1/ConsentFormActivity.java
+++ app/src/main/java/com/example/cliqv1/ConsentFormActivity.java
@@ -8,7 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class AcceptedConsentFormActivity extends AppCompatActivity {
+public class ConsentFormActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
