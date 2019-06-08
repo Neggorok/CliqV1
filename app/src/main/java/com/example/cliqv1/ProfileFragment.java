f6eefadc2fad897491e3e3ad38b08110e0f97c63
vfroehlich <valeria.froehlich@stud.hshl.de>
2019-06-08 13:30:23 +0200

    Einverständniserklärung für Profil eingefügt

--- app/src/main/java/com/example/cliqv1/ProfileFragment.java
+++ app/src/main/java/com/example/cliqv1/ProfileFragment.java
@@ -6,4 +6,9 @@ import android.os.Bundle;
 
 public class ProfileFragment extends Fragment {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_fragment);
+
 }
