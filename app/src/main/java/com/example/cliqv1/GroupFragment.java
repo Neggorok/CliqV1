4ac3c23c6c4e79d970553ab763019856b007bf7d
vfroehlich <valeria.froehlich@stud.hshl.de>
2019-06-08 16:39:27 +0200

    Navigation Bar, Login funktioniert gerade nicht, kann nicht testen

--- app/src/main/java/com/example/cliqv1/GroupFragment.java
+++
@@ -1,9 +0,0 @@
-package com.example.cliqv1;
-
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class GroupFragment extends Fragment {
-
-}
