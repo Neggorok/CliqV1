27af9a02a897fe697ca3fc770c2c6ff760f2c7ef
Neggorok <alexhermes87@gmx.de>
2019-06-14 01:11:42 +0200

    Reparatur Chat

--- app/src/main/java/com/example/cliqv1/ChatActivity.java
+++ app/src/main/java/com/example/cliqv1/ChatActivity.java
@@ -111,7 +111,7 @@ public class ChatActivity extends AppCompatActivity {
 
         messageList.clear();
 
-        String create_user_url = getString(R.string.cliq) + "/getMessagesForChat.php";
+        String create_user_url = getString(R.string.cliq) + "/getMessagesForChat_cliq.php";
 
         StringRequest postRequest = new StringRequest(Request.Method.POST, create_user_url,
 
